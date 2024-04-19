10b12e4ecadeb0b376acb24fc371ed661adcaad4
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-04-18 22:22:32 -0400

    Added a Teleport to VIP for testing

---
+++ Assets/Scripts/VIP Lounge/TeleportToLoungeButton.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportToLoungeButton : UdonSharpBehaviour {
+	[Tooltip("The event message to send on a player interact event.")]
+	public string interactEvent;
+	private VRCPlayerApi playerLocal;
+	public GameObject VIPLounge;
+
+	void Start() {
+		playerLocal = Networking.LocalPlayer;
+	}
+
+	public override void Interact() {
+		playerLocal.TeleportTo(VIPLounge.transform.position, VIPLounge.transform.rotation);
+	}
+}
